dbcea8750d259a09aae17d1155d8c717e34a7975
Joe Kurleto <joedoku@gmail.com>
2015-09-17 18:03:15 -0500

    Add get_and_update to MyAgent

--- my_agent.exs
+++ my_agent.exs
@@ -8,7 +8,7 @@ defmodule MyAgent do
     send(pid, {:get, func, self})
 
     receive do
-      {:get, response} ->
+      {:response, response} ->
         response
     after 5000 ->
       IO.puts "timed out"
@@ -20,13 +20,27 @@ defmodule MyAgent do
     :ok
   end
 
+  def get_and_update(pid, func) do
+    send(pid, {:get_and_update, func, self})
+
+    receive do
+      {:response, response} ->
+        response
+    after 5000 ->
+      IO.puts "timed out"
+    end
+  end
+
   defp agent_loop(state) do
     receive do
       {:get, func, from} ->
         response = func.(state)
-        send(from, {:get, response})
+        send(from, {:response, response})
       {:update, func} ->
         state = func.(state)
+      {:get_and_update, func, from} ->
+        {response, state} = func.(state)
+        send(from, {:response, response})
     end
     agent_loop(state)
   end
